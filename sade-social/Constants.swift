1acee820adb5fe3f1c275843fdec6e21c824bca2
Dilyan Ergeledzhiev <dergeledji@gmail.com>
2017-07-05 14:40:08 +0200

    Sign in ui custom classes somplete

---
+++ sade-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  sade-social
+//
+//  Created by sade on 5/7/17.
+//  Copyright © 2017 sade. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
