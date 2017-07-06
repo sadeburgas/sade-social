b384aa1f2305ef8bbca16e66ea5d0e0c023f9614
Dilyan Ergeledzhiev <dergeledji@gmail.com>
2017-07-06 23:16:21 +0200

    Design Feed UI Complete

--- sade-social/CircleView.swift
+++ sade-social/CircleView.swift
@@ -1,24 +1,25 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  sade-social
 //
-//  Created by sade on 5/7/17.
+//  Created by sade on 6/7/17.
 //  Copyright © 2017 sade. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
-        super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-       
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
